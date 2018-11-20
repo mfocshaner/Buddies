a03b7958b0a20ad53d4d3f16b7fad45c98343048
Michael Jacob Focshaner <mfocshaner@lightricks.com>
2018-11-20 22:49:09 +0200

    CreateEventActivity: add event model.

--- app/src/main/java/com/huji/foodtricks/buddies/CreateEventActivity.java
+++ app/src/main/java/com/huji/foodtricks/buddies/CreateEventActivity.java
@@ -3,6 +3,9 @@ package com.huji.foodtricks.buddies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Date;
+import java.util.List;
+
 public class CreateEventActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +13,38 @@ public class CreateEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
     }
+
+    /// parameters to be passed to new event
+    private Date _time;
+    private String _eventType; // not well defined
+    private List<String> _invitees; // will maybe change if invitees become "People" objects,
+
+    private void chooseGroup(String groupName) {
+        // set invitees to be the group
+    }
+
+    private void chooseTime(Date time){
+        _time = time;
+    }
+
+    private void chooseEventType(String eventType) {
+        _eventType = eventType;
+    }
+
+    private String makeTitle(){
+        String title = _eventType.concat("At ").concat(_time.toString());
+        return title;
+    }
+
+    /**
+     * Called when "create event" button is clicked
+     * should be enabled only if all parameters are chosen (disable button before that)
+     * @return EventModel with all chosen parameters
+     */
+    private EventModel createEvent(){
+        String title = makeTitle();
+        EventModel newEvent = new EventModel(title, _time, _eventType, _invitees);
+        return newEvent;
+    }
+
 }
